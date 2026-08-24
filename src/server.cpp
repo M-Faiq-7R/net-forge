ce64d51cb8fadaf7043c1c965ec1babc1ace6976
m-faiq-7r <mfaiq.cs@gmail.com>
2026-08-24 17:54:23 +0500

    Completed Server Side Code -- Added Accept() so server actually accepts and communicates with client

--- src/server.cpp
+++ src/server.cpp
@@ -41,12 +41,17 @@ int main() {
     }
 
     // Listening for TCP connections
-    if(listen(server_socket , backlogs) == 0){
+    if(listen(server_socket , backlogs) == 0){                                       // Listen doesn't actually communicates with client , instead it just waits for user request to form connection , we use accept() to form connection with client-server.
         std::cout << "Server listening ..." << std::endl;
     }else{
         std::cout << "Server Failed to listen ... " << std::endl;
     }
 
+    // Accepting Clients TCP connection request
+    sockaddr_in client_address;
+    socklen_t client_address_length = sizeof(client_address);
+    int client_socket =  accept(server_socket , (struct sockaddr*)&client_address , &client_address_length);  // Parameters of accept are : (server_socket, client_address, client_address_length)
+    std::cout << "Connection Accepted! " << std::endl;
 
     draw_line(2);         
     return 0;
