e1a1ca375432afff11173cad7da3f489f0737440
m-faiq-7r <mfaiq.cs@gmail.com>
2026-08-24 21:55:27 +0500

    Completed Client side socket request

--- src/client.cpp
+++ src/client.cpp
@@ -3,19 +3,30 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string>
+#include <cerrno>
+#include <cstring>
 
 int main(){
-    std::string ip_addr = "127.0.0.1";
+    std::string ip_addr = "127.0.0.1";  // IPv4 to which client will connect
 
     int client_socket = socket(AF_INET , SOCK_STREAM, 0);
     std::cout << "Client socket created Successfully ..." << std::endl;
 
+    // Assigning addrs to clients socket
     sockaddr_in server_address ;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080); 
     
-    inet_pton(AF_INET, ip_addr.c_str() , &server_address.sin_addr);
+    // Turns human read-able ip into binary(linux read-able)
+    int server_ipv4_binary_address_conversion_status = inet_pton(AF_INET, ip_addr.c_str() , &server_address.sin_addr);
 
-
+    // Connecting with the server
+    int connection_status = connect(client_socket , (struct sockaddr*)&server_address , sizeof(server_address) );
+    if (connection_status == 0){
+        std::cout << "Connected to Server!" << std::endl;
+    }else{
+        std::cout << "Failed to connect to server!" << std::endl;
+        std::cout << "Error : " << strerror(errno) << std::endl;
+    }
     return 0;
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "utils.h"
 
-void draw_line(int i=1){
+void draw_line(int i){
     for(int j =0; j<i; j++){
         std::cout << "===================================================================" << std::endl;
     }
