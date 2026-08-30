8b94871ebc70b11ad54bde8be3d7eb01953dad73
m-faiq-7r <mfaiq.cs@gmail.com>
2026-08-30 12:51:53 +0500

    Moved Handling Client code into another file

--- src/thread_handler.cpp
+++ src/thread_handler.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include "thread_handler.h"
 #include <thread>
+#include <cstring>
+#include <netinet/in.h>
 
-#include "utils.h"
 
-void nigga(){
-    std::thread t1(draw_line , 2);
-    std::thread t2(draw_x_line , 3);
-    t2.join();
-    t1.join();
+void handle_client(int client_socket){
+    // Sending message to client
+        std::string message = "Hello Client! ";
+        if(send(client_socket , message.c_str() , message.size() , 0) == -1){
+            std::cout << "Failed to send message" << std::endl;
+        }
+        
+
+        // Recieving message from client
+        while (true){
+            char c_message[1024];
+            int byte_received = recv(client_socket, c_message , sizeof(c_message) -1 , 0);
+            if (byte_received > 0){
+                c_message[byte_received] = '\0';
+                std::cout << "Client : " << c_message << std::endl;
+                std::string message = "Message Recieved by Server!";
+                if (send(client_socket , message.c_str() , message.size() , 0) == -1){
+                    std::cout << "Failed to send message" << std::endl;
+                    break;
+                }
+            }
+            else if(byte_received == 0){
+                std::cout << "Client closed its connection." << std::endl;
+                break;
+            }
+            else{
+                std::cout << "Error : " << strerror(errno) << std::endl;
+                break;
+            }
+        }
 }
 
-int main(){
-    nigga();
-    return 0;
-}
