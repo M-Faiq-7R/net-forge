9788983b6b4c6a7841e1b7f9b6489b6e6cd2da51
m-faiq-7r <mfaiq.cs@gmail.com>
2026-08-31 20:25:09 +0500

    Introduced Record of Connected Sockets

--- src/thread_handler.cpp
+++ src/thread_handler.cpp
@@ -5,10 +5,15 @@
 #include <netinet/in.h>
 #include <vector>
 
+std::vector<int> connected_sockets;  // Vector list created to keep record of all connected sockets.
 
 void handle_client(int client_socket){
     // Sending message to client
         std::string message = "Hello Client! ";
+        connected_sockets.push_back(client_socket);   // Append Client socket at the end of the vector
+        for(int i =0 ; i <= connected_sockets.size() ; i++){       // For testing purposes
+            std::cout << connected_sockets[i] << " , ";      
+        }
         if(send(client_socket , message.c_str() , message.size() , 0) == -1){
             std::cout << "Failed to send message" << std::endl;
         }
@@ -29,6 +34,7 @@ void handle_client(int client_socket){
             }
             else if(byte_received == 0){
                 std::cout << "Client closed its connection." << std::endl;
+                // std::erase(connected_sockets , client_socket);
                 break;
             }
             else{
