59621162a2f9f45633d2fc65773d1e22d3db25c3
m-faiq-7r <mfaiq.cs@gmail.com>
2026-09-02 22:01:57 +0500

    Used mutex_lockGuard to avoif race condition

--- src/thread_handler.cpp
+++ src/thread_handler.cpp
@@ -5,23 +5,29 @@
 #include <netinet/in.h>
 #include <vector>
 #include <algorithm>
+#include <mutex>                 // Library added to use lockguard funtion
 
 std::vector<int> connected_sockets;  // Vector list created to keep record of all connected sockets.
+std::mutex my_mutex ;
 
 void remove_client_socket(int client_socket , std::vector<int> connected_client_sockets){
-    auto it = std::find(connected_sockets.begin(), connected_sockets.end() , client_socket);   // this will move through whole vector and then {it} will be the index of desired number
-    if (it != connected_sockets.end()){
-        connected_sockets.erase(it);
+    auto it = std::find(connected_client_sockets.begin(), connected_client_sockets.end() , client_socket);   // this will move through whole vector and then {it} will be the index of desired number
+    if (it != connected_client_sockets.end()){
+        connected_client_sockets.erase(it);
     }
 }
 
 void handle_client(int client_socket){
     // Sending message to client
         std::string message = "Hello Client! ";
-        connected_sockets.push_back(client_socket);   // Append Client socket at the end of the vector
+        {   // This block only allows one thread to access and modify this vector at a time to avoid race condition
+            std::lock_guard<std::mutex> lock(my_mutex);
+            connected_sockets.push_back(client_socket);   // Append Client socket at the end of the vector
+        }
         if(send(client_socket , message.c_str() , message.size() , 0) == -1){
             std::cout << "Failed to send message" << std::endl;
         }
+        
         
 
         // Recieving message from client
@@ -39,7 +45,10 @@ void handle_client(int client_socket){
             }
             else if(byte_received == 0){
                 std::cout << "Client closed its connection." << std::endl;
-                remove_client_socket(client_socket , connected_sockets);        // Removing Disconnected Clients
+                {   // This block only allows one thread to access and modify this vector at a time to avoid race condition
+                    std::lock_guard<std::mutex> lock(my_mutex);
+                    remove_client_socket(client_socket , connected_sockets);        // Removing Disconnected Clients 
+                }
                 break;
             }
             else{
