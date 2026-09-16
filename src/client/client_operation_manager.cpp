a8292c9e90fda53953aa786f24b432e8914b0fc2
m-faiq-7r <mfaiq.cs@gmail.com>
2026-09-16 23:27:36 +0500

    Fixed a bug

--- src/client/client_operation_manager.cpp
+++ src/client/client_operation_manager.cpp
@@ -27,19 +27,6 @@ void rcv_data(int client_socket){
         
 }
 
-void send_data(int client_socket){
-    char c_message[1024];
-        int byte_received = recv(client_socket, c_message , sizeof(c_message) -1 , 0);
-        if (byte_received > 0){
-            c_message[byte_received] = '\0';
-            std::cout << "Client : " << c_message << std::endl;
-            std::string message = "Message Recieved by Client!";
-            if (send(client_socket , message.c_str() , message.size() , 0) == -1){
-                std::cout << "Failed to send message" << std::endl;
-            }
-        }
-}
-
 void handle_client_input(int client_socket){
     while (true){
         std::string c_message;
@@ -58,9 +45,6 @@ void handle_client_input(int client_socket){
                     std::cout << "Failed to send message to server " << std::endl;
                     running = false;
                     break;
-                }else{
-                    send_data(client_socket);
-                    running = true;
                 }
             }
         }
